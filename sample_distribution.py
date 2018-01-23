48398d72e141d28afe881c56eaf0c44ede9d27e1
OscarJavierHernandez <javierhernandezmelgar@gmail.com>
2018-01-23 17:26:37 +0100

    added more code

--- sample_distribution.py
+++ sample_distribution.py
@@ -6,20 +6,39 @@
 import numpy as np
 from scipy.optimize import minimize
 import random
+from scipy.integrate import quad
 
 class arbitrary_distribution:
 	
-	def __init__(self,func):
+	#--------------------------------------------------------------
+	# The constructor for the arbitrary distribution sample generator
+	# Three arguments:
+	# 1. the desired distribution profile
+	# 2. the minimum sample value,
+	# 3. the maximum sample value
+	#--------------------------------------------------------------
+	def __init__(self,func,a,b):
 		self.func = func
+		self.a = a
+		self.b = b
+		self.Norm = quad(self.func, a, b)[0]
+		
+	#--------------------------------------------------------------
+	# This function defines the cummulative distribution of a function
+	# Int[F(y),{y,-Infinity,x}]
+	#--------------------------------------------------------------
+	def cumulative_distribution(self,x):
+		s = quad(self.func, self.a, x)[0]/self.Norm
+		return s
 	
 	#--------------------------------------------------------------
 	# This function inverts, by finding the value of x which
-	# minimizes the value abs(y-func(x))
+	# minimizes the value (y-func(x))**2
 	#--------------------------------------------------------------
 	def invert_function(self,yi):
 		
 		def residual(x):
-			return (yi-self.func(x))**2
+			return (yi-self.cumulative_distribution(x))**2
 		
 		# Minimize the residual using Nelder-Mead method
 		x0 = yi
@@ -30,21 +49,58 @@ class arbitrary_distribution:
 		return xi
 	
 	#--------------------------------------
-	# Evaluate the function
+	# Evaluate the Normalized input function
 	#--------------------------------------
 	def eval_func(self,x):
-		return self.func(x)
+		return self.func(x)/self.Norm
 	
-	#--------------------------------------
-	#
-	#--------------------------------------
+	#-------------------------------------------------------------------
+	# Compute the moment of the target PDF distribution
+	#-------------------------------------------------------------------
+	def eval_PDF_moment(self,k):
+		
+		def kernel(x):
+			return self.eval_func(x)*(x**k)
+		
+		s = quad(kernel, self.a, self.b)[0]
+		
+		return s
+	
+	# Compute the moment of a sample
+	def eval_sample_moment(self,v,k):
+		'''
+		Arguments:
+		k = the order of the moment
+		v = the vector containing the sample values
+		'''
+		
+		return s
+	
+	#-------------------------------------------------------------------
+	# This function generates a single sample with target distribution.
+	# The method employed here is known as the: 
+	#-------------------------------------------------------------------
 	def draw_sample(self):
 		
 		u = random.uniform(0, 1)
 		yi = self.invert_function(u)[0]
 		
 		return yi
+	
+	#-------------------------------------------------------------------
+	# Draw a large number of samples
+	#-------------------------------------------------------------------
+	def draw_samples(self,N):
 		
+		# Initialize the vector of samples
+		v = np.zeros(N)
+		
+		# Draw N samples from the target distrobution
+		v = [self.draw_sample() for i in range(0,N)]
+		
+		v = np.asarray(v)
+		
+		return v
 		
 	
 	
--- test.py
+++ test.py
@@ -4,13 +4,21 @@ import matplotlib.pyplot as plt
 
 
 def func(x):
-	return np.exp(-x)
+	return x*np.exp(-50*(x-0.2)**2)+np.exp(-60*(x-0.6)**2)
+	#return (x-0.4)**2
+	
+a0 = 0.0
+b0 = 1.0
+dist1 = sd.arbitrary_distribution(func,a0,b0)
 
-dist1 = sd.arbitrary_distribution(func)
+x = np.arange(a0,b0,0.01)
+a = dist1.draw_samples(80000)
 
-a = [dist1.draw_sample() for i in range(500)]
+for k in range(1,50):
+	print 'm('+str(k)+')', np.mean(a**k), dist1.eval_moment(k)
 
-plt.hist(a, bins='auto')  # arguments are passed to np.histogram
+exit()
+plt.hist(a, bins='auto',normed=True,alpha=0.6)  # arguments are passed to np.histogram
+plt.plot(x,dist1.eval_func(x),'-', linewidth=2)
 plt.title("Histogram with 'auto' bins")
 plt.show()
-#plt.show()
